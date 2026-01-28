6845efd8342d6dd78541dd0e632863c45b69dc47
Atilio3108 <salassistema03@gmail.com>
2026-01-28 14:50:49 -0300

    versiones de dependencias

--- Salas/vite.config.js
+++ Salas/vite.config.js
@@ -1,10 +1,8 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
@@ -15,4 +13,11 @@ export default defineConfig({
       '@': fileURLToPath(new URL('./src', import.meta.url))
     },
   },
+  // --- AGREGAMOS ESTO ---
+  build: {
+    // Apunta a la carpeta public de tu repositorio de Backend
+    outDir: '../../backEnd/public', 
+    // Limpia los archivos viejos antes de generar el nuevo build
+    emptyOutDir: true, 
+  }
 })
